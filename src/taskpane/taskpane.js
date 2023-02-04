2baa288ee38f645124d95ca86d6a3beea27856a4
DefterYu <a73014@outlook.com>
2023-02-05 01:14:15 +0800

    小改动

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -6,25 +6,41 @@
 /* global document, Office, Word */
 
 Office.onReady((info) => {
-  if (info.host === Office.HostType.Word) {
-    document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
-  }
+    if (info.host === Office.HostType.Word) {
+        document.getElementById("sideload-msg").style.display = "none";
+        document.getElementById("app-body").style.display = "flex";
+        document.getElementById("insert-paragraph").onclick = insertParagraph;
+    }
 });
 
+async function insertParagraph() {
+    await Word.run(async (context) => {
+        const docBody = context.document.body;
+        docBody.insertParagraph(
+            "Office has several versions, including Office 2016, Microsoft 365 subscription, and Office on the web.",
+            "Start"
+        );
+        await context.sync();
+    }).catch(function (error) {
+        console.log("Error: " + error);
+        if (error instanceof OfficeExtension.Error) {
+            console.log("Debug info: " + JSON.stringify(error.debugInfo));
+        }
+    });
+}
+
 export async function run() {
-  return Word.run(async (context) => {
-    /**
-     * Insert your Word code here
-     */
+    return Word.run(async (context) => {
+        /**
+         * Insert your Word code here
+         */
 
-    // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+        // insert a paragraph at the end of the document.
+        const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
 
-    // change the paragraph color to blue.
-    paragraph.font.color = "blue";
+        // change the paragraph color to blue.
+        paragraph.font.color = "blue";
 
-    await context.sync();
-  });
+        await context.sync();
+    });
 }
